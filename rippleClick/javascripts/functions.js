65b064c7c6a525d6d6cf02110be8e5ade0fa86c1
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-10-18 22:02:43 -0400

    it didn't work, have to try more things

--- rippleClick/javascripts/functions.js
+++ rippleClick/javascripts/functions.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
   
+  $('.ripple-button').click(function(e){
+    var parent = $(this).parent();
+
+      //create ripple if it doesnt exist
+      if (parent.find('.ripple').length == 0) {
+        parent.prepend('<span class="ripple"></span>');
+      }
+
+      var ripple = parent.find('.ripple');
+      ripple.removeClass('animate');
+      if (!ripple.height() && !ripple.width()) {
+        var dim = Math.max(parent.outerHeight(), parent.outerWidth());
+        ripple.css({
+          height: dim,
+          width: dim
+        });
+      }
+
+      var x = e.pageX - parent.offset().left - ripple.width()/2;
+      var y = e.pageY - parent.offset().top - ripple.height()/2;
+      ripple
+        .css({
+          top: y + 'px',
+          left: x + 'px'
+        })
+        .addClass('animate');
+  });
+
 });
