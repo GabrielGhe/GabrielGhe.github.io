255656aa91a49f562dcc237776f1c1247eb52a8b
Gabriel Ghe <GabrielGhe@users.noreply.github.com>
2015-04-23 12:51:16 -0700

    Update search.js

--- search.js
+++ search.js
@@ -2,40 +2,45 @@
 ---
 /** @jsx React.DOM */
 
-var random_posts = [];
-
-// init lunr
-var idx = lunr(function () {
-  this.field('title', 10);
-  this.field('category', 8);
-  this.field('tags', 5);
-  this.field('date', 4);
-  this.field('content');
-});
-
-// add each document to be index
-for(var index in data) {
-  idx.add(data[index]);
-  random_posts.push(index);
-  delete data[index].content;
-  delete data[index].date;
-}
-
-var shuffle = function(v){
-    for(var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
-    return v;
-}
-
-random_posts = shuffle(random_posts).splice(0,5);
-random_posts = random_posts.map(function(key){
-  return {
-      id: data[key].id,
-      title: data[key].title,
-      category: data[key].category
-  };
-});
-
 $(function() {
+  var random_posts = [];
+  var data = {
+    {% for post in site.posts %}
+      {% include custom/post.json %},
+    {% endfor %}
+  };
+  
+  // init lunr
+  var idx = lunr(function () {
+    this.field('title', 10);
+    this.field('category', 8);
+    this.field('tags', 5);
+    this.field('date', 4);
+    this.field('content');
+  });
+  
+  // add each document to be index
+  for(var index in data) {
+    idx.add(data[index]);
+    random_posts.push(index);
+    delete data[index].content;
+    delete data[index].date;
+  }
+  
+  var shuffle = function(v){
+      for(var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+      return v;
+  }
+  
+  random_posts = shuffle(random_posts).splice(0,5);
+  random_posts = random_posts.map(function(key){
+    return {
+        id: data[key].id,
+        title: data[key].title,
+        category: data[key].category
+    };
+  });
+  
   React.render(<SearchBar lunr={idx} posts={data} />, document.getElementById('searchbar'));
   React.render(<RandomPostList posts={random_posts} />, document.getElementById('random_posts'));
 });
